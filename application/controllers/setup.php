b00b5aac58e6ef3f3b5be46aad54891104e69edd
boyernotseaboyer <sboyer@airvm.com>
2015-10-20 14:14:45 -0300

    ...

---
+++ application/controllers/setup.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class setup extends MY_Controller {
+				public function __construct()	{
+								parent::__construct();
+								$this->load->database();
+				}
+				
+				public function resetDatabase() {
+								$this->splitNrun(file_get_contents(APPPATH . "scripts/master.sql"));
+								$this->splitNrun(file_get_contents(APPPATH . "scripts/defaultData.sql"));
+								
+								echo "Done.";
+				}
+				
+				private function splitNrun($sql) {
+								$sqls = explode(';', $sql);
+								array_pop($sqls);
+
+								foreach($sqls as $statement){
+												$statement .=  ";";
+												$this->db->query($statement);   
+								}
+				}
+}
