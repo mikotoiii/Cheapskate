0824cd18c88729da89b3aaf8a9893c7754758d60
boyernotseaboyer <sboyer@airvm.com>
2015-10-20 15:59:48 -0300

    Try this...

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://cheapskate-boytimes.rhcloud.com/';
+$config['base_url'] = ''; //http://cheapskate-boytimes.rhcloud.com/
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class home extends MY_Controller {
+class Home extends MY_Controller {
 
 				public function index() {
 								$this->load->view('home');
