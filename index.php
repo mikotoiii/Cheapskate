3d8a4d4be9df638d5031e7d7d61b24d9556fde05
boyernotseaboyer <sboyer@airvm.com>
2015-10-28 12:58:56 -0300

    Update

--- index.php
+++ index.php
@@ -65,12 +65,13 @@ define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'developm
  */
 switch (ENVIRONMENT) {
     case 'development':
-        error_reporting(-1);
+    case 'production':
+        error_reporting(E_ALL); // -1
         ini_set('display_errors', 1);
         break;
 
     case 'testing':
-    case 'production':
+    //case 'production':
         ini_set('display_errors', 0);
         if (version_compare(PHP_VERSION, '5.3', '>=')) {
             error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
