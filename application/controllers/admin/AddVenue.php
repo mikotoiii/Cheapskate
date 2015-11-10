56c457b00838b591eea7b4d9c5b8c6ed171169cf
boyernotseaboyer <sboyer@airvm.com>
2015-11-10 15:49:04 -0400

    Added first admin page

---
+++ application/controllers/admin/AddVenue.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Sign up for this thing. Handle the post-facebook/google
+ * signup.
+ */
+class AddVenue extends MY_Controller {
+    
+				public function __construct()	{
+								parent::__construct();
+				}
+				
+				public function index() {
+    
+    }
+    
+}
