879bcb8d286ab07f9ef0eeb69c2829358fe422bb
mikotoiii <boyer.steve@gmail.com>
2015-11-20 09:33:18 -0400

    Cleaned up the formatting on the template files

--- application/views/login.php
+++ application/views/login.php
@@ -1,37 +1,37 @@
 <div class="wrapper style1">
-				<article id="work">
-								<header>
-												<h2>Login!</h2>
+    <article id="work">
+        <header>
+            <h2>Login!</h2>
             <div class="alert-error">
                 <?= validation_errors(); ?>
             </div>
-								</header>
-								<div class="container 50%">
-												<section>
-																<form method="post" action="/login">
-																				<div>
-																								<div class="row">
-																												<div class="6u">
-																																<input type="text" name="username" id="username" placeholder="username or email" value="<?= set_value('username'); ?>"/>
-																												</div>
+        </header>
+        <div class="container 50%">
+            <section>
+                <form method="post" action="/login">
+                    <div>
+                        <div class="row">
                             <div class="6u">
-																																<input type="password" name="password" id="password" placeholder="password" />
-																												</div>
-																								</div>
-																								<div class="row">
-																												<div class="12u">
-																																<ul class="actions">
-																																				<li><input type="submit" value="Sign in!" /></li>
-																																</ul>
-																												</div>
-																								</div>
-																				</div>
-																</form>
-																<footer>
-																				<div>...or sign in with Facebook!</div>
-																				<fb:login-button scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button>
-																</footer>
-												</section>
-								</div>
-				</article>
+                                <input type="text" name="username" id="username" placeholder="username or email" value="<?= set_value('username'); ?>"/>
+                            </div>
+                            <div class="6u">
+                                <input type="password" name="password" id="password" placeholder="password" />
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="12u">
+                                <ul class="actions">
+                                    <li><input type="submit" value="Sign in!" /></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <footer>
+                    <div>...or sign in with Facebook!</div>
+                        <fb:login-button scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button>
+                </footer>
+            </section>
+        </div>
+    </article>
 </div>
--- application/views/signup.php
+++ application/views/signup.php
@@ -1,71 +1,74 @@
 <div class="wrapper style1">
-				<article id="work">
-								<header>
-												<h2>Signup!</h2>
-								</header>
-								<div class="container 50%">
-												<section>
-																<form method="post" action="/signup">
-																				<div>
+    <article id="work">
+        <header>
+            <h2>Signup!</h2>
+            <div class="alert-error">
+                <?= validation_errors(); ?>
+            </div>
+        </header>
+        <div class="container 50%">
+            <section>
+                <form method="post" action="/signup">
+                    <div>
                         <div class="row">
                             <div class="12u">
                                 <input type="text" name="username" id="username" placeholder="username" />
                             </div>
                         </div>
                         <div class="row">
-                            	<div class="12u">
-																																<input type="text" name="email" id="email" placeholder="email" />
-																												</div>
+                            <div class="12u">
+                                <input type="text" name="email" id="email" placeholder="email" />
+                            </div>
                         </div>
-																								<div class="row">
-																												<div class="6u 12u(mobile)">
-																																<input type="text" name="nameFirst" id="nameFirst" placeholder="first name" />
-																												</div>
-																											<div class="6u 12u(mobile)">
-																																<input type="text" name="nameLast" id="nameLast" placeholder="last name" />
-																												</div>
-																								</div>
-																								<div class="row">
-																												<div class="6u 12u(mobile)">
-																																<input type="password" name="password1" id="subject" placeholder="password" />
-																												</div>
-																												<div class="6u 12u(mobile)">
-																																<input type="password" name="passwordconf" id="subject" placeholder="confirm password" />
-																												</div>
-																								</div>
+                        <div class="row">
+                            <div class="6u 12u(mobile)">
+                                <input type="text" name="nameFirst" id="nameFirst" placeholder="first name" />
+                            </div>
+                            <div class="6u 12u(mobile)">
+                                <input type="text" name="nameLast" id="nameLast" placeholder="last name" />
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="6u 12u(mobile)">
+                                <input type="password" name="password1" id="subject" placeholder="password" />
+                            </div>
+                            <div class="6u 12u(mobile)">
+                                <input type="password" name="passwordconf" id="subject" placeholder="confirm password" />
+                            </div>
+                        </div>
                         <div class="row">
                             <div class="6u">
-																																<input type="text" name="dob" id="dob" placeholder="birth date" />
-																												</div>
+                                <input type="text" name="dob" id="dob" placeholder="birth date" />
+                            </div>
                             <div class="6u">
                                 <label for="dob">(...helps us show you birthday-specific deals!)</label>
                             </div>
                         </div>
-																								<div class="row">
-																												<div class="12u">
-																																<ul class="actions">
-																																				<li><input type="submit" value="Sign up!" /></li>
-																																</ul>
-																												</div>
-																								</div>
-																				</div>
-																</form>
-																<footer>
-																				<div>...or register with Facebook!</div>
-																				<fb:login-button scope="public_profile,email,user_friends" onlogin="checkLoginState();"></fb:login-button>
-																</footer>
-												</section>
-								</div>
-				</article>
+                        <div class="row">
+                            <div class="12u">
+                                <ul class="actions">
+                                    <li><input type="submit" value="Sign up!" /></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <footer>
+                    <div>...or register with Facebook!</div>
+                    <fb:login-button scope="public_profile,email,user_friends" onlogin="checkLoginState();"></fb:login-button>
+                </footer>
+            </section>
+        </div>
+    </article>
 </div>
 
 <script type='text/javascript'>
-    $(function() {
-        $("#dob").datepicker({
-            changeMonth: true,
-            changeYear: true,
-            yearRange: "-70:-19",
-            defaultDate: "-19y"
-        });
+$(function() {
+    $("#dob").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        yearRange: "-70:-19",
+        defaultDate: "-19y"
     });
+});
 </script>
